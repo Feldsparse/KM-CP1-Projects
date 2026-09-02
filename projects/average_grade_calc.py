dffd648cfd057d8dff908dc166dfa60e687c2d24
Feldsparse <charles.motta@ucas-edu.net>
2026-09-02 09:35:55 -0600

    edfkkjeiy

--- projects/average_grade_calc.py
+++ projects/average_grade_calc.py
@@ -7,7 +7,7 @@ num_four = float(input("what is your grade in your fourth class?: "))
 num_five = float(input("what is your grade in your fifth class?: "))
 num_six = float(input("what is your grade in your sixth class?: "))
 num_seven = float (input("what is your grade in your seventh class?: "))
-Average = float(num_one + num_two + num_three + num_four + num_five + num_six + num_seven / 7 )
-final_num = round(Average,2)
-
-print("your average grade is " + Average )
+Average = (float (num_one) + (num_two) + (num_three) + (num_four) + (num_five) + (num_six) + (num_seven)) / 7
+average= round(Average,2)
+print("your final grade is ")
+print(average)
